c56c771aeb103945bd5d12e485bb012a20a32ae6
Alka Rai <mrsalka02@gmail.com>
2024-10-22 18:38:44 +0545

    login page added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,57 @@
 import React from "react";
 
-function SimpleComponent() {
+const Page = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-      <p>Hello Learning Utsav</p>
+    <div className="bg-gray-100 flex items-center justify-center h-screen">
+      <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h1 className="text-center text-2xl font-bold mb-6">Login</h1>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 "
+              id="username"
+              type="text"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow border rounded w-full py-2 px-3 text-gray-700 mb-3 "
+              id="password"
+              type="password"
+              placeholder="********"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+              type="button"
+            >
+              Sign In
+            </button>
+          </div>
+          <div className="flex items-center justify-between">
+            Don't have an accout yet ?
+            <a href="#" className="text-blue-500 underline">
+              sign up
+            </a>
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
-export default SimpleComponent;
+export default Page;
