9b677e102275cbefb01f18341bb2859ea70dc615
Alka Rai <mrsalka02@gmail.com>
2024-10-26 20:07:37 +0545

    skills page added

---
+++ src/app/skills/page.js
@@ -0,0 +1,84 @@
+import CustonNavBar from "@/component/NavBar/page";
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div className=" font-bold">
+      <CustonNavBar></CustonNavBar>
+      <div className="bg-pink-50 flex items-center justify-center h-screen">
+        <div className="max-w-4xl bg-white shadow-lg rounded-lg p-8 text-center">
+          <h1 className="text-3xl font-bold text-pink-600 mb-6">My Skills</h1>
+
+          {/* Skills Grid */}
+          <div className="grid grid-cols-3 gap-8">
+            {/* HTML */}
+            <div className="flex flex-col items-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+                alt="HTML"
+                className="w-16 h-16"
+              />
+              <p className="text-gray-700 mt-2">HTML</p>
+            </div>
+            {/* CSS */}
+            <div className="flex flex-col items-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+                alt="CSS"
+                className="w-16 h-16"
+              />
+              <p className="text-gray-700 mt-2">CSS</p>
+            </div>
+            {/* React.js */}
+            <div className="flex flex-col items-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+                alt="React"
+                className="w-16 h-16"
+              />
+              <p className="text-gray-700 mt-2">React.js</p>
+            </div>
+            {/* JavaScript */}
+            <div className="flex flex-col items-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+                alt="JavaScript"
+                className="w-16 h-16"
+              />
+              <p className="text-gray-700 mt-2">JavaScript</p>
+            </div>
+            {/* Next.js */}
+            <div className="flex flex-col items-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg"
+                alt="Next.js"
+                className="w-16 h-16"
+              />
+              <p className="text-gray-700 mt-2">Next.js</p>
+            </div>
+            {/* MongoDB */}
+            <div className="flex flex-col items-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg"
+                alt="MongoDB"
+                className="w-16 h-16"
+              />
+              <p className="text-gray-700 mt-2">MongoDB</p>
+            </div>
+            {/* Tailwind CSS */}
+            <div className="flex flex-col items-center">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg"
+                alt="Tailwind CSS"
+                className="w-16 h-16"
+              />
+              <p className="text-gray-700 mt-2">Tailwind CSS</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
--- src/component/NavBar/page.js
+++ src/component/NavBar/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Navbar,
@@ -9,18 +10,24 @@ import {
 } from "@nextui-org/react";
 
 const CustonNavBar = () => {
+  const handleGitHubClick = () => {
+    window.open("https://github.com/aalkaraii", "_blank");
+  };
   return (
     <div>
       <Navbar>
         <NavbarBrand>
-          <div className="">
-            <p className="font-bold text-inherit">ALAKA RAI</p>
-            <p> Student at KEC</p>
-          </div>
+          <NavbarItem>
+            <Link color="foreground" href="/#">
+              <p className="font-bold text-inherit text-pink-500">ALAKA RAI</p>
+              <br></br>
+              <p> Student at KEC</p>
+            </Link>
+          </NavbarItem>
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
           <NavbarItem>
-            <Link color="foreground" href="#">
+            <Link color="foreground" href="/skills">
               skills
             </Link>
           </NavbarItem>
@@ -37,16 +44,12 @@ const CustonNavBar = () => {
         </NavbarContent>
         <NavbarContent justify="end">
           <NavbarItem color="foreground">
-            <Link href="#">GitHub</Link>
+            <Link href="#" onClick={handleGitHubClick}>
+              GitHub
+            </Link>
           </NavbarItem>
           <NavbarItem>
-            <Button
-              as={Link}
-              color="pink"
-              href="#"
-              variant="flat"
-              className="text-pink-600"
-            >
+            <Button as={Link} color="pink" href="/contactus" variant="flat">
               Email
             </Button>
           </NavbarItem>
